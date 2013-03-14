f793ec3bd27b1057539511cc07a523cdd523be34
Seancito <info@themeic.com>
2013-03-14 21:20:46 +0000

    removed farb.js enqueue, rgba enabled

--- options/fields/color/field_color.php
+++ options/fields/color/field_color.php
@@ -20,47 +20,17 @@ class Redux_Options_color {
      * Takes the vars and outputs the HTML for the field in the settings
      *
      * @since Redux_Options 1.0.0
+     * 
+     * Themeic edit for rgba
     */
-    function render() {
+     function render() {
         $class = (isset($this->field['class'])) ? $this->field['class'] : '';
-
-        if(get_bloginfo('version') >= '3.5') {
-            echo '<input type="text" id="' . $this->field['id'] . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" value="' . $this->value . '" class="' . $class . ' popup-colorpicker" style="width: 70px;" data-default-color="' . esc_attr($this->value) . '"/>';
+	
+            echo '<div class="rgba-color-picker color ' . $class . '" data-color="' . $this->value . '" data-color-format="rgba">
+		<span class="add-on"><i style="background-color: ' . $this->value . '"></i></span><br>
+		<input type="text" id="' . $this->field['id'] . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" value="' . $this->value . '" class="' . $class . '" /></div>';
             echo (isset($this->field['desc']) && !empty($this->field['desc'])) ? ' <span class="description">' . $this->field['desc'] . '</span>' : '';
-        } else {
-            echo '<div class="farb-popup-wrapper">';
-            echo '<input type="text" id="' . $this->field['id'] . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" value="' . $this->value . '" class="' . $class . ' popup-colorpicker" style="width:70px;"/>';
-            echo '<div class="farb-popup"><div class="farb-popup-inside"><div id="' . $this->field['id'] . 'picker" class="color-picker"></div></div></div>';
-            echo (isset($this->field['desc']) && !empty($this->field['desc'])) ? ' <span class="description">' . $this->field['desc'] . '</span>' : '';
-            echo '</div>';
-        }
+       
     }
 
-    /**
-     * Enqueue Function.
-     *
-     * If this field requires any scripts, or css define this function and register/enqueue the scripts/css
-     *
-     * @since Redux_Options 1.0.0
-    */
-    function enqueue() {
-        if(get_bloginfo('version') >= '3.5') {
-            wp_enqueue_style('wp-color-picker');
-            wp_enqueue_script(
-                'redux-opts-field-color-js',
-                Redux_OPTIONS_URL . 'fields/color/field_color.js',
-                array('wp-color-picker'),
-                time(),
-                true
-            );
-        } else {
-            wp_enqueue_script(
-                'redux-opts-field-color-js', 
-                Redux_OPTIONS_URL . 'fields/color/field_color_farb.js', 
-                array('jquery', 'farbtastic'),
-                time(),
-                true
-            );
-        }
-    }
 }
