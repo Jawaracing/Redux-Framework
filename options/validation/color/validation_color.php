e93afdff5760e227105a75a2e25709f2f7408b0e
Seancito <info@themeic.com>
2013-03-14 21:14:35 +0000

    Remove color validation for HEX colors

--- options/validation/color/validation_color.php
+++ options/validation/color/validation_color.php
@@ -1,13 +1,13 @@
 <?php
 class Redux_Validation_color {
-	
+// this color validation does not work with rgba colors	
 	/**
 	 * Field Constructor.
 	 *
 	 * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
 	 *
 	 * @since Redux_Options 1.0.0
-	*/
+	
 	function __construct($field, $value, $current) {
 		$this->field = $field;
 		$this->field['msg'] = (isset($this->field['msg'])) ? $this->field['msg'] : __('This field must be a valid color value.', Redux_TEXT_DOMAIN);
@@ -22,7 +22,7 @@ class Redux_Validation_color {
 	 * Takes the vars and outputs the HTML for the field in the settings
 	 *
 	 * @since Redux_Options 1.0.0
-	*/
+	
 	function validate() {
 		if(!is_array($this->value)) {
 			if($this->value[0] != '#') {
@@ -53,4 +53,5 @@ class Redux_Validation_color {
 			}
         }
 	}
+*/
 }
